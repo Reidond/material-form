8377452e81c730259c974eb7d6f71feb36a61169
Andrey Shafar <reidond@gmail.com>
2019-10-05 23:42:15 +0300

    Done with it

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Checkout from './components/Checkout';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <Checkout />;
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
